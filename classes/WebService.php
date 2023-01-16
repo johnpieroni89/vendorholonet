00774771e59c8da5bbada85fcdd32fc4092d7f5a
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 10:06:47 -0600

    test

--- classes/WebService.php
+++ classes/WebService.php
@@ -21,9 +21,7 @@ class WebService {
         $count = $resp->vendors->{'@attributes'}->{'count'};
         $start = $resp->vendors->{'@attributes'}->{'start'};
 
-        $count_vendors = 0;
         while(($start - 1) + $count <= $total) {
-            //var_dump($count.' : '.$start.' : '.$total);
             foreach ($resp->vendors->vendor as $vendor) {
                 $vendor_id = $vendor->{'@attributes'}->{'id'};
                 $vendor_resp = $this->fetch_api(self::VENDOR_API . '/' . $vendor_id);
@@ -50,9 +48,7 @@ class WebService {
             $total = $resp->vendors->{'@attributes'}->{'total'};
             $count = $resp->vendors->{'@attributes'}->{'count'};
             $start = $resp->vendors->{'@attributes'}->{'start'};
-            $count_vendors++;
         }
-        echo $count_vendors;
     }
 
 
