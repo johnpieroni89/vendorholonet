dd91cac15d61b8e1bbf85585e24d38cb55e67772
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 02:40:55 -0600

    test

--- classes/Vendor.php
+++ classes/Vendor.php
@@ -35,7 +35,7 @@ class Vendor {
     static function getAll() {
         $db = new Database();
         $db->connect();
-        $vendorIdList = mysqli_fetch_object(mysqli_query($db->connection, "SELECT id FROM vendors ORDER BY id"));
+        $vendorIdList = mysqli_fetch_all(mysqli_query($db->connection, "SELECT id FROM vendors ORDER BY id"));
         var_dump($vendorIdList);
         return
         $vendorArr = array();
