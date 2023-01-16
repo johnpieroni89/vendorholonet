282e289e2ce3b4841af621bf1db0bd10d697506e
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 02:48:26 -0600

    test

--- classes/UserInterface.php
+++ classes/UserInterface.php
@@ -115,7 +115,7 @@ class UserInterface
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        '.$_SESSION['handle'].'
                     </div>
                 </nav>
             </div>
@@ -148,6 +148,31 @@ class UserInterface
             <script src="assets/demo/chart-bar-demo.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
             <script src="js/datatables-simple-demo.js"></script>
+        ';
+    }
+
+    static function printCharts() {
+        echo '
+            <div class="row">
+                <div class="col-xl-6">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-chart-area me-1"></i>
+                            Area Chart Example
+                        </div>
+                        <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+                    </div>
+                </div>
+                <div class="col-xl-6">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-chart-bar me-1"></i>
+                            Bar Chart Example
+                        </div>
+                        <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+                    </div>
+                </div>
+            </div>
         ';
     }
 }
