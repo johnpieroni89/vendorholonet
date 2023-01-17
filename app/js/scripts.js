53745d84243d28efabcf9a32f5ee238a93737508
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 20:02:31 -0600

    test

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -26,5 +26,10 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 $(document).ready(function () {
-    $('#vendorTable').DataTable();
+    $('#vendorTable').DataTable({
+        lengthMenu: [
+            [10, 25, 50, -1],
+            [10, 25, 50, 'All'],
+        ],
+    });
 });
