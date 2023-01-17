fc38d32fce16be38587ffb1c6df0b080d8dc7196
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 21:22:19 -0600

    test

--- classes/Vendor.php
+++ classes/Vendor.php
@@ -102,7 +102,7 @@ class Vendor {
             echo '
                 <tr>
                     <td>'.$vendor->id.'</td>
-                    <a href="#"><td>'.$vendor->name.'</td></a>
+                    <td>'.$vendor->name.'</td>
                     <td>'.$vendor->owner.'</td>
                     <td>'.count($wares).'</td>
                     <td>0</td>
