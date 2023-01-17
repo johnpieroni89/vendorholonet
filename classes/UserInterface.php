cac8723b20eda0d72e7342dc27fcf74c1901d1a9
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 19:36:48 -0600

    test

--- classes/UserInterface.php
+++ classes/UserInterface.php
@@ -45,7 +45,7 @@ class UserInterface
                 <!-- Navbar-->
                 <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-gear"></i></a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="../index.php">Logout</a></li>
                         </ul>
