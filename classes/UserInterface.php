d5856cc07d37a37cc476f0884db9c2667c412def
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 19:13:34 -0600

    test

--- classes/UserInterface.php
+++ classes/UserInterface.php
@@ -20,6 +20,7 @@ class UserInterface
                 <title>Vendor Holonet</title>
                 <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
                 <link href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css" rel="stylesheet" />
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
                 <link href="css/styles.css" rel="stylesheet" />
                 <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
             </head>
