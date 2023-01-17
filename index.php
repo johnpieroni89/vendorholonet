9a7dbb2d0bb5a73331e7405bd8451b3347df54c6
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 19:24:48 -0600

    test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['handle'])) {
 }
 
 ?>
-<body style="background-color: #6c757d">
+<body style="background-color: antiquewhite">
     <!-- Login Container -->
     <div class="d-flex align-items-center justify-content-center" style="width: 100%; height: 100%; margin: auto;">
         <!-- Login Block -->
