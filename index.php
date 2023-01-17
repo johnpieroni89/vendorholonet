26f091c88f99dfcbea02fe980e1b51279d6ac174
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 19:00:29 -0600

    test

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php
 
 include('autoload.php');
+UserInterface::printHead();
 if(!isset($_SESSION['handle'])) {
     header("Location: oauth/index.php");
 } else {
     header("Location: app/index.php");
 }
+
+?>
+
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+        <!-- Tabs Titles -->
+
+        <!-- Icon -->
+        <div class="fadeIn first">
+            <img src="app/assets/img/logo-large.png" id="logo" alt="Vendor Holonet" />
+        </div>
+
+        <!-- Login Form -->
+        <div class="d-grid gap-2">
+            <a href="oauth/index.php"><button type="button" class="btn btn-lg btn-secondary">Login</button></a>
+        </div>
+
+    </div>
+</div>
