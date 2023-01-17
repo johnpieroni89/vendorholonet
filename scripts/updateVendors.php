1218242544c4e96697242819fdc95b8456d09f1f
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-17 15:55:54 -0600

    test

--- scripts/updateVendors.php
+++ scripts/updateVendors.php
@@ -1,12 +1,12 @@
 <?php
 
-include('../config.php');
-include('../classes/Database.php');
-include('../classes/User.php');
-include('../classes/Vendor.php');
-include('../classes/Location.php');
-include('../classes/Ware.php');
-include('../classes/WebService.php');
+include(__DIR__.'/../config.php');
+include(__DIR__.'/../classes/Database.php');
+include(__DIR__.'/../classes/User.php');
+include(__DIR__.'/../classes/Vendor.php');
+include(__DIR__.'/../classes/Location.php');
+include(__DIR__.'/../classes/Ware.php');
+include(__DIR__.'/../classes/WebService.php');
 
 $ws = new WebService();
 $ws->updateVendorList();
