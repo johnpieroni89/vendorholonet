e210792ecdf514e3d8c7aa90a204554a1cd47c46
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-16 19:56:02 -0600

    test

--- classes/UserInterface.php
+++ classes/UserInterface.php
@@ -98,6 +98,7 @@ class UserInterface
         echo '
             <!-- Bootstrap 5 JS --> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
             <!-- JQuery 3 + DataTables JS --> <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.1/af-2.5.1/b-2.3.3/cr-1.6.1/date-1.2.0/fc-4.2.1/fh-3.3.1/kt-2.8.0/r-2.4.0/rg-1.3.0/rr-1.3.1/sc-2.0.7/sb-1.4.0/sp-2.1.0/sl-1.5.0/sr-1.2.0/datatables.min.js"></script>
+            <!-- Default JS --> <script type="text/javascript" src="js/scripts.js"></script>
         ';
     }
 
