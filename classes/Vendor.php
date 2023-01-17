7ee8f93ba727e42886ca0792c8128f24727b9014
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-17 16:09:06 -0600

    test

--- classes/Vendor.php
+++ classes/Vendor.php
@@ -148,7 +148,7 @@ class Vendor {
             <hr/>
             
             <div class="container-fluid">
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5"><center>
         ';
 
         $wares = Ware::getVendorWares($vendor);
@@ -168,7 +168,7 @@ class Vendor {
         }
 
         echo '
-                </div>
+                </center></div>
             </div>
         ';
     }
