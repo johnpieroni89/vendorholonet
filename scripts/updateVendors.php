2db54b6ae573f63104750d57340d7d0543ec0df4
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-18 00:15:57 -0600

    test

--- scripts/updateVendors.php
+++ scripts/updateVendors.php
@@ -8,5 +8,8 @@ include(__DIR__.'/../classes/Location.php');
 include(__DIR__.'/../classes/Ware.php');
 include(__DIR__.'/../classes/WebService.php');
 
+/*
 $ws = new WebService();
 $ws->updateVendorList();
+*/
+var_dump(Vendor::getAll());
