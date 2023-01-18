2204da818de771f4727d2b5c07fd1f0754385848
johnpieroni89 <john.pieroni89@gmail.com>
2023-01-18 13:38:09 -0600

    test

--- classes/Vendor.php
+++ classes/Vendor.php
@@ -77,8 +77,7 @@ class Vendor {
         $owner = mysqli_real_escape_string($db->connection, $owner);
         str_replace('&amp;', '&', $name);
 
-        var_dump("INSERT INTO vendors (id, `name`, merchant, merchant_img, description, owner, ) VALUES ('$id', '$name', '$merchant', '$merchant_img', '$description', '$owner')");
-        mysqli_query($db->connection, "INSERT INTO vendors (id, `name`, merchant, merchant_img, description, owner, ) VALUES ('$id', '$name', '$merchant', '$merchant_img', '$description', '$owner')");
+        mysqli_query($db->connection, "INSERT INTO vendors (id, `name`, merchant, merchant_img, description, owner) VALUES ('$id', '$name', '$merchant', '$merchant_img', '$description', '$owner')");
     }
 
     static function printVendorTable($vendors) {
