7b9dca41d998d7f842fd9d1ab547b9f37812af4e
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-18 00:10:52 -0600

    test

--- classes/WebService.php
+++ classes/WebService.php
@@ -63,7 +63,7 @@ class WebService {
                             $quantity = str_replace(',', '', $ware['quantity']);
                             Ware::parseWare($vendor_id, $ware['type'], $quantity, $ware['price'], $ware['currency'], $ware['images']['small'], $ware['images']['large']);
                         } else {
-                            var_dump(false);
+                            var_dump($ware['type']);
                         }
                     }
                 }
