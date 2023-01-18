f1c90f305b888e253ff21786ce0160a37c7584a6
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-17 19:19:41 -0600

    test

--- classes/WebService.php
+++ classes/WebService.php
@@ -17,37 +17,38 @@ class WebService {
     {
         Vendor::deleteAll();
         $resp = $this->fetch_api(self::VENDOR_API);
-        $total = $resp->vendors->{'@attributes'}->{'total'};
-        $count = $resp->vendors->{'@attributes'}->{'count'};
-        $start = $resp->vendors->{'@attributes'}->{'start'};
+        $resp = $resp['swcapi']['vendors'];
+        $total = $resp['attributes']['total'];
+        $count = $resp['attributes']['count'];
+        $start = $resp['attributes']['start'];
 
         while(($start - 1) + $count <= $total) {
-            foreach ($resp->vendors->vendor as $vendor) {
-                $vendor_id = $vendor->{'@attributes'}->{'id'};
+            foreach ($resp['vendor'] as $vendor) {
+                $vendor_id = $vendor['attributes']['id'];
                 $vendor_resp = $this->fetch_api(self::VENDOR_API . '/' . $vendor_id .'.json');
-                $vendor_data = $vendor_resp->vendor;
+                $vendor_data = $vendor_resp['swcapi']['vendor'];
 
-                if (is_string($vendor_data->{'description'})) {
-                    Vendor::parseVendor($vendor_data->{'id'}, $vendor_data->{'name'}, $vendor_data->{'description'}, $vendor_data->{'owner'});
+                if (is_string($vendor_data['description'])) {
+                    Vendor::parseVendor($vendor_data['id'], $vendor_data['name'], $vendor_data['description'], $vendor_data['owner']);
                 } else {
-                    Vendor::parseVendor($vendor_data->{'id'}, $vendor_data->{'name'}, '', $vendor_data->{'owner'});
+                    Vendor::parseVendor($vendor_data['id'], $vendor_data['name'], '', $vendor_data['owner']);
                 }
 
                 Ware::deleteVendorWares($vendor_id);
-                foreach ($vendor_data->wares as $ware) {
+                foreach ($vendor_data['wares'] as $ware) {
                     if ($ware) {
-                        if ($vendor_id && isset($ware->{'type'}) && $ware->{'quantity'} && $ware->{'price'} && $ware->{'currency'} && $ware->{'images'}->{'small'} && $ware->{'images'}->{'large'}) {
-                            $quantity = str_replace(',', '', $ware->{'quantity'});
-                            Ware::parseWare($vendor_id, $ware->{'type'}, $quantity, $ware->{'price'}, $ware->{'currency'}, $ware->{'images'}->{'small'}, $ware->{'images'}->{'large'});
+                        if ($vendor_id && isset($ware['type']) && $ware['quantity'] && $ware['price'] && $ware['currency'] && $ware['images']['small'] && $ware['images']['large']) {
+                            $quantity = str_replace(',', '', $ware['quantity']);
+                            Ware::parseWare($vendor_id, $ware['type'], $quantity, $ware['price'], $ware['currency'], $ware['images']['small'], $ware['images']['large']);
                         }
                     }
                 }
             }
 
             $resp = $this->fetch_api(self::VENDOR_API.'.json?start_index='.($start + 50));
-            $total = $resp->vendors->{'@attributes'}->{'total'};
-            $count = $resp->vendors->{'@attributes'}->{'count'};
-            $start = $resp->vendors->{'@attributes'}->{'start'};
+            $total = $resp['attributes']['total'];
+            $count = $resp['attributes']['count'];
+            $start = $resp['attributes']['start'];
         }
     }
 
