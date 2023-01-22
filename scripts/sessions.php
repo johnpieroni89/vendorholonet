c47ccda0117b38e7657cf39685d20f18b87d53f8
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-21 23:54:06 -0600

    session handler in restricted folder for server admin to check

--- scripts/sessions.php
+++ scripts/sessions.php
@@ -6,8 +6,8 @@ $arr = scandir(session_save_path());
 
 foreach($arr as $data){
     $session = file_get_contents('/var/lib/php/sessions/'.$data);
-    if($data) {
-        var_dump($data);
+    if($session) {
+        var_dump($session);
     }
 }
 
