fb3a56c1a0c24c4352ad0f7bb49bee5f2a09d047
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-22 00:58:04 -0600

    session handler in restricted folder for server admin to check

--- app/admin/sessions.php
+++ app/admin/sessions.php
@@ -20,55 +20,56 @@ if(!$_SESSION['handle'] == CONFIG_GLOBAL_ADMIN) {
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid px-4">
-                <h1 class="mt-4">Malls</h1>
+                <h1 class="mt-4">Sessions</h1>
                 <hr/>
                 <div class="container-fluid">
                     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
                         <?php
                             $db = new Database();
-                            $db->connect;
+                            $db->connect();
                             $sessionsSQL = mysqli_query($db->connection, "SELECT * FROM sessions ORDER BY date_active DESC");
 
-                        echo '
-                            <div class="card mb-4">
-                                <div class="card-header">
-                                    <i class="fas fa-table me-1"></i>
-                                    Active Sessions
-                                </div>
-                                <div class="card-body">
-                                    <table id="vendorTable" class="table table-sm table-striped table-responsive table-hover table-bordered">
-                                        <thead>
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Handle</th>
-                                                <th>Activity Timestamp</th>
-                                            </tr>
-                                        </thead>
-                                        <tfoot>
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Handle</th>
-                                                <th>Activity Timestamp</th>
-                                            </tr>
-                                        </tfoot>
-                                        <tbody>';
+                            echo '
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <i class="fas fa-table me-1"></i>
+                                        Active Sessions
+                                    </div>
+                                    <div class="card-body">
+                                        <table id="vendorTable" class="table table-sm table-striped table-responsive table-hover table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Handle</th>
+                                                    <th>Activity Timestamp</th>
+                                                </tr>
+                                            </thead>
+                                            <tfoot>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Handle</th>
+                                                    <th>Activity Timestamp</th>
+                                                </tr>
+                                            </tfoot>
+                                            <tbody>
+                            ';
 
-                            while($row = mysqli_fetch_object($sessionsSQL)) {
-                                echo '
-                                    <tr>
-                                        <td>'.$row->id.'</td>
-                                        <td>'.$row->handle.'</td>
-                                        <td>'.date('d M Y H:i:s', $row->date_active).'</td>
-                                    </tr>
-                                ';
-                            }
+                                while($row = mysqli_fetch_object($sessionsSQL)) {
+                                    echo '
+                                        <tr>
+                                            <td>'.$row->id.'</td>
+                                            <td>'.$row->handle.'</td>
+                                            <td>'.date('d M Y H:i:s', $row->date_active).'</td>
+                                        </tr>
+                                    ';
+                                }
 
-                        echo '                    
-                                        </tbody>
-                                    </table>
+                            echo '                    
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
-                            </div>
-                        ';
+                            ';
                         ?>
                     </div>
                 </div>
