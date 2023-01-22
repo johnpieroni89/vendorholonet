6d1a2954b15be2f213cf5890f0691223dc3f584d
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-21 23:51:56 -0600

    session handler in restricted folder for server admin to check

--- scripts/sessions.php
+++ scripts/sessions.php
@@ -3,7 +3,7 @@
 //print_r(scandir(session_save_path()));
 
 $arr = scandir(session_save_path());
-var_dump(file_get_contents('/var/lib/php/sessions/'.$arr[5]));
+var_dump(file_get_contents('/var/lib/php/sessions/'.$arr[58]));
 
 //phpinfo();
 
