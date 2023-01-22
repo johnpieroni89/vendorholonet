a45b9e04203a08c78f37ead16cc2fcbd6b1f665f
johnpieroni89 <johnpieroni89@gmail.com>
2023-01-22 00:46:52 -0600

    session handler in restricted folder for server admin to check

--- classes/UserInterface.php
+++ classes/UserInterface.php
@@ -33,7 +33,7 @@ class UserInterface
                 <title>Vendor Holonet</title>
                 <!-- Bootstrap 5 CSS --> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
                 <!-- JQuery 3 + DataTables CSS --> <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.1/af-2.5.1/b-2.3.3/cr-1.6.1/date-1.2.0/fc-4.2.1/fh-3.3.1/kt-2.8.0/r-2.4.0/rg-1.3.0/rr-1.3.1/sc-2.0.7/sb-1.4.0/sp-2.1.0/sl-1.5.0/sr-1.2.0/datatables.min.css"/>
-                <!-- Default CSS --> <link href="'.UserInterface::siteURL().'/app/assets/css/styles.css" rel="stylesheet" />
+                <!-- Default CSS --> <link href="'.UserInterface::siteURL().'/app/css/styles.css" rel="stylesheet" />
                 <!-- FontAwesome --> <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
             </head>
         ';
@@ -119,7 +119,7 @@ class UserInterface
         echo '
             <!-- Bootstrap 5 JS --> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
             <!-- JQuery 3 + DataTables JS --> <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.1/af-2.5.1/b-2.3.3/cr-1.6.1/date-1.2.0/fc-4.2.1/fh-3.3.1/kt-2.8.0/r-2.4.0/rg-1.3.0/rr-1.3.1/sc-2.0.7/sb-1.4.0/sp-2.1.0/sl-1.5.0/sr-1.2.0/datatables.min.js"></script> <!-- https://datatables.net/examples/advanced_init/dom_multiple_elements.html -->
-            <!-- Default JS --> <script type="text/javascript" src="'.UserInterface::siteURL().'/app/assets/js/scripts.js"></script>
+            <!-- Default JS --> <script type="text/javascript" src="'.UserInterface::siteURL().'/app/js/scripts.js"></script>
             <!-- Chart.js --> <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script> <!-- https://www.w3schools.com/js/js_graphics_chartjs.asp -->
         ';
     }
