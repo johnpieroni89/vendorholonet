bb8561c1590cac95e91d6278deb3b714c79e23fb
Jeroen Wasteels <jeroenwasteels@gmail.com>
2023-09-01 20:29:35 +0200

    Added download functionality to wares lists

--- app/wares.php
+++ app/wares.php
@@ -41,9 +41,11 @@ if(!isset($_SESSION['handle'])) {
                         <?php
                             if(isset($_GET['type'])) {
                                 $wares = Ware::getWaresByType($_GET['type']);
+                                echo "<div><form action='download.php'><input type='hidden' name='type' value='".$_GET['type']."'><button type='submit' class='btn btn-primary' name='download' value='download'>Download</button></form></div>";
                                 Ware::printWareTable($wares);
                             } elseif(isset($_GET['container_uid'])) {
                                 $wares = Ware::getWaresByContainer($_GET['container_uid']);
+                                echo "<div><form action='download.php'><input type='hidden' name='type' value='".$_GET['type']."'><button type='submit' class='btn btn-primary' name='download' value='download'>Download</button></form></div>";
                                 Ware::printWareTable($wares);
                             } else {
                                 echo '<center><h5>Please select a type in the form above!</h5></center>';
